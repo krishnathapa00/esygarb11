8368e1d3bf40911dd8a113eacb82a42bbe603dd0
CodeAlchemist <undeadbacteria@gmail.com>
2025-08-11 17:58:34 +0545

    Update ProductDetails.tsx

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -35,7 +35,7 @@ const ProductDetails = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 pb-20">
       <Header />
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
