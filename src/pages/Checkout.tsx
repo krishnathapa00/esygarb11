b0ea81bc09c840a7c567c0e21fef94a34ea35d7d
CodeAlchemist <undeadbacteria@gmail.com>
2025-09-07 19:43:25 +0545

    Update Checkout.tsx

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -29,7 +29,8 @@ const Checkout = () => {
   const location = useLocation();
 
   const OFFICE_COORDS = { lat: 27.687441, lng: 85.340829 };
-  const MAX_DISTANCE_KM = 2;
+  const MAX_DISTANCE_KM = 1.5;
+
   const [deliveryCoords, setDeliveryCoords] = useState(null);
   const [isWithinRange, setIsWithinRange] = useState(true);
 
