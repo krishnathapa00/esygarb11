2c6559d380cb5f23a58767fe5be75695aa0c1e97
CodeAlchemist <undeadbacteria@gmail.com>
2025-09-26 11:58:28 +0545

    Update OrderConfirmation.tsx

--- src/pages/OrderConfirmation.tsx
+++ src/pages/OrderConfirmation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import { ArrowLeft, CheckCircle } from "lucide-react";
