ec258ca906cf4f662df5963ce4c08fb55143f369
CodeAlchemist <undeadbacteria@gmail.com>
2025-12-16 16:50:02 +0545

    Add files via upload

---
+++ src/pages/admin/index.ts
@@ -0,0 +1,20 @@
+// Admin Pages
+export { default as AddProduct } from "./AddProduct";
+export { default as AddProductNew } from "./AddProductNew";
+export { default as AdminDashboard } from "./AdminDashboard";
+export { default as AdminLogin } from "./AdminLogin";
+export { default as AssignDarkstore } from "./AssignDarkstore";
+export { default as AssignDelivery } from "./AssignDelivery";
+export { default as AssignOrder } from "./AssignOrder";
+export { default as DeliveryList } from "./DeliveryList";
+export { default as DeliveryPartnerManagement } from "./DeliveryPartnerManagement";
+export { default as DeliverySettings } from "./DeliverySettings";
+export { default as ManageCategories } from "./ManageCategories";
+export { default as ManageDarkstores } from "./ManageDarkstores";
+export { default as ManageKYC } from "./ManageKYC";
+export { default as ManageOrders } from "./ManageOrders";
+export { default as ManageProducts } from "./ManageProducts";
+export { default as ManagePromoCodes } from "./ManagePromoCodes";
+export { default as ManageUsers } from "./ManageUsers";
+export { default as OrderDetails } from "./OrderDetails";
+export { default as Transactions } from "./Transactions";
